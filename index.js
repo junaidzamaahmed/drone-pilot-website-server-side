d15bc9d596b22fb3f0820e541d521ea8fc4b0c79
junaidzamaahmed <junaidahmedzama@gmail.com>
2021-11-13 18:47:34 +0600

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 const ObjectId = require("mongodb").ObjectId;
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,9 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://junaid:tvvinthinking@cluster0.xk9pn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xk9pn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
